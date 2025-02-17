be2c5154f70b52edbcd3e02691eecfc8666c45a4
emilabbasov06 <emilabb13@gmail.com>
2025-02-17 19:32:11 +0400

    worked on api

--- api/routers/istilik.py
+++ api/routers/istilik.py
@@ -11,8 +11,17 @@ router = APIRouter(
 
 
 @router.get('/')
-def products(db: Session = Depends(get_db)):
+def get_products(db: Session = Depends(get_db)):
   products = db.query(IstilikVeHavalandirmaModel).all()
   if not products:
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail='Found nothing inside the database')
   return products
+
+
+@router.get('/{id}')
+def get_product(id: int, db: Session = Depends(get_db)):
+  product = db.query(IstilikVeHavalandirmaModel).filter(IstilikVeHavalandirmaModel.id == id).first()
+  if not product:
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'There was no product with that specific id: {id}')
+  
+  return product
